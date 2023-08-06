acc3e495810c42fb93f1ac2b9c0e6787b6dfb7c0
Mack Solomon <macksol@gmail.com>
2023-08-06 13:05:07 -0700

    changes

--- src/main/java/org/tictactoe/AiPlayer.java
+++ src/main/java/org/tictactoe/AiPlayer.java
@@ -17,34 +17,23 @@ package org.tictactoe;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 import org.tictactoe.TicTacToe.*;
 
 public class AiPlayer {
 
-  private static List<List<Cell>> board;
-
-  private static List<Position> possibleMoves(List<List<Cell>> board) {
-    return board.stream()
-        .flatMap(
-            row ->
-                row.stream()
-                    .filter(cell -> cell.getPlayer().equals(Player.NONE))
-                    .map(Cell::getPosition))
-        .collect(Collectors.toList());
-  }
+  private static List<List<Cell>> localBoard;
 
   public static Position nextMoveMiniMax(List<List<Cell>> board, Player player) {
-    AiPlayer.board = board;
+    localBoard = board;
     Position result = new Position(0, 0);
     int bestScore = player.equals(Player.O) ? Integer.MIN_VALUE : Integer.MAX_VALUE;
 
-    for (int row = 0; row < AiPlayer.board.size(); row++) {
-      for (int column = 0; column < AiPlayer.board.get(row).size(); column++) {
-        if (AiPlayer.board.get(row).get(column).getPlayer() == Player.NONE) {
-          AiPlayer.board = Board.nextBoard(AiPlayer.board, new Position(row, column), Player.O);
+    for (int row = 0; row < localBoard.size(); row++) {
+      for (int column = 0; column < localBoard.get(row).size(); column++) {
+        if (localBoard.get(row).get(column).getPlayer() == Player.NONE) {
+          localBoard = Board.nextBoard(localBoard, new Position(row, column), Player.O);
           int score = cycleMiniMax(0, false);
-          AiPlayer.board = Board.nextBoard(AiPlayer.board, new Position(row, column), Player.NONE);
+          localBoard = Board.nextBoard(localBoard, new Position(row, column), Player.NONE);
 
           if ((player.equals(Player.O) && score > bestScore)
               || (player.equals(Player.X) && score < bestScore)) {
@@ -59,39 +48,37 @@ public class AiPlayer {
   }
 
   private static int cycleMiniMax(int depth, boolean isMaximizingPlayer) {
-    if (GameOps.getWinner(AiPlayer.board).equals(Optional.of(Player.O))) {
+    if (GameOps.getWinner(localBoard).equals(Optional.of(Player.O))) {
       return 1;
     }
-    if (GameOps.getWinner(AiPlayer.board).equals(Optional.of(Player.X))) {
+    if (GameOps.getWinner(localBoard).equals(Optional.of(Player.X))) {
       return -1;
     }
-    if (Board.isBoardFull(AiPlayer.board)) {
+    if (Board.isBoardFull(localBoard)) {
       return 0;
     }
 
     int bestScore;
     if (isMaximizingPlayer) {
       bestScore = Integer.MIN_VALUE;
-      for (int row = 0; row < AiPlayer.board.size(); row++) {
-        for (int column = 0; column < AiPlayer.board.get(row).size(); column++) {
-          if (AiPlayer.board.get(row).get(column).getPlayer() == Player.NONE) {
-            AiPlayer.board = Board.nextBoard(AiPlayer.board, new Position(row, column), Player.O);
+      for (int row = 0; row < localBoard.size(); row++) {
+        for (int column = 0; column < localBoard.get(row).size(); column++) {
+          if (localBoard.get(row).get(column).getPlayer() == Player.NONE) {
+            localBoard = Board.nextBoard(localBoard, new Position(row, column), Player.O);
             int score = cycleMiniMax(depth + 1, false);
-            AiPlayer.board =
-                Board.nextBoard(AiPlayer.board, new Position(row, column), Player.NONE);
+            localBoard = Board.nextBoard(localBoard, new Position(row, column), Player.NONE);
             bestScore = Math.max(bestScore, score);
           }
         }
       }
     } else {
       bestScore = Integer.MAX_VALUE;
-      for (int row = 0; row < AiPlayer.board.size(); row++) {
-        for (int column = 0; column < AiPlayer.board.get(row).size(); column++) {
-          if (AiPlayer.board.get(row).get(column).getPlayer() == Player.NONE) {
-            AiPlayer.board = Board.nextBoard(AiPlayer.board, new Position(row, column), Player.X);
+      for (int row = 0; row < localBoard.size(); row++) {
+        for (int column = 0; column < localBoard.get(row).size(); column++) {
+          if (localBoard.get(row).get(column).getPlayer() == Player.NONE) {
+            localBoard = Board.nextBoard(localBoard, new Position(row, column), Player.X);
             int score = cycleMiniMax(depth + 1, true);
-            AiPlayer.board =
-                Board.nextBoard(AiPlayer.board, new Position(row, column), Player.NONE);
+            localBoard = Board.nextBoard(localBoard, new Position(row, column), Player.NONE);
             bestScore = Math.min(bestScore, score);
           }
         }
